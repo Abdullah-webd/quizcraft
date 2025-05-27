8b88f8106a1a16d165af63c9459f212fea45f573
WebMaster <ajalabdllh@gmail.com>
2025-05-27 20:29:37 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://quizcraft-8rh7.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
